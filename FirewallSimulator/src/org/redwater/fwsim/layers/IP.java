497487ce849283455c1ed624eb2b308e18d4ff4a
ghelmer <ghelmer@iastate.edu>
2018-08-13 21:28:08 -0500

    Remove unused methods from layers classes.

--- FirewallSimulator/src/org/redwater/fwsim/layers/IP.java
+++ FirewallSimulator/src/org/redwater/fwsim/layers/IP.java
@@ -1,41 +1,4 @@
 package org.redwater.fwsim.layers;
 
 public class IP implements IPacket {
-
-	@Override
-	public Byte[] getPayload() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setPayload(Byte[] payload) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Byte[] getHeader() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setHeader(Byte[] payload) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setId() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- FirewallSimulator/src/org/redwater/fwsim/layers/IPacket.java
+++ FirewallSimulator/src/org/redwater/fwsim/layers/IPacket.java
@@ -1,10 +1,4 @@
 package org.redwater.fwsim.layers;
 
 public interface IPacket {
-	public Byte[] getPayload();
-	public void setPayload(Byte[] payload);
-	public Byte[] getHeader();
-	public void setHeader(Byte[] payload);
-	public String getId();
-	public void setId();
 }
